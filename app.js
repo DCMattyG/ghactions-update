cf200eaa43aa921342040426f3cec102abd7ef0e
Matthew Garrett <magarret@microsoft.com>
2024-02-13 13:05:38 -0800

    Added auto-reload features

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var livereload = require("livereload");
+var connectLiveReload = require("connect-livereload");
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
 var app = express();
+
+app.use(connectLiveReload());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const packageJson = require('../package.json');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', version: packageJson.version });
 });
 
 module.exports = router;
